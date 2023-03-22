1e4cb7742dc82ad3c0c5ae4e97b47fea57101711
Ipinmi Adebayo <61703527+ipinmi@users.noreply.github.com>
2023-03-22 21:26:37 +0100

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
+""" State Module for AirBnB clone project """
 from models.base_model import BaseModel, Base
 from models import storage_type
 from models.city import City
